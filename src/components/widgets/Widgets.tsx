70cba34fb776b942059b5913d72a317eb7c186a1
Nikolay Nesterchuk <nikolay.nesterchuk@powerplay.com>
2025-06-06 14:40:13 +0300

    remove delete widget

--- src/components/widgets/Widgets.tsx
+++ src/components/widgets/Widgets.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, ReactNode, useState } from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import CalendarWidget from './components/CalendarWidget';
 import TimeWidget from './components/TimeWidget';
@@ -15,68 +15,24 @@ const Wrapper = styled.div`
 `;
 
 
-export const Container = styled.div`
-  position: relative;
-  &:hover .delete-button {
-    opacity: 1;
-  }
-`;
-
-
-export const DeleteButton = styled.button`
-  opacity: 0;
-  position: absolute;
-  top: -10px;
-  right: -10px;
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  background-color: #ff4444;
-  color: white;
-  border: none;
-  cursor: pointer;
-  z-index: 1000;
-  padding: 0;
-  line-height: 1;
-  transition: all 0.3s ease-in-out;
-  
-  &:hover {
-    background-color: #ff0000;
-  }
-`;
+const WIDGETS = [
+  {size: 6, widget: <TimeWidget />},
+  {size: 6, widget: <CalendarWidget />},
+  {size: 12, widget: <VideoWidget />},
+  {size: 12, widget: <ContactsWidget />},
+  {size: 6, widget: <Fragment />},
+  {size: 6, widget: <UnitedWidget />},
+]
 
 const Widgets = () => {
-  const [data, setData] = useState<{size: number; widget: ReactNode}[]>([
-    {size: 6, widget: <TimeWidget />},
-    {size: 6, widget: <CalendarWidget />},
-    {size: 12, widget: <VideoWidget />},
-    {size: 12, widget: <ContactsWidget />},
-    {size: 6, widget: <Fragment />},
-    {size: 6, widget: <UnitedWidget />},
-  ]);
-
-  const handleDelete = (index: number) => {
-    setData(prevData => prevData.filter((_, i) => i !== index));
-  };
-
   return (
     <Wrapper>
       <Box display={'flex'} justifyContent={'end'}>
         <Box maxWidth={370} padding={2}>
           <Grid container spacing={2}>
-            {data.map((item, index) => (
+            {WIDGETS.map((item, index) => (
               <Grid size={item.size} key={index}>
-                <Container>
-                  {item.widget}
-                  {item.widget && (
-                    <DeleteButton
-                      className="delete-button"
-                      onClick={() => handleDelete(index)}
-                    >
-                      ×
-                    </DeleteButton>
-                  )}
-                </Container>
+                {item.widget}
               </Grid>
             ))}
           </Grid>
